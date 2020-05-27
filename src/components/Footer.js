729a9f62518bb0059c6e64ef049940460e3f6a08
EDowning2000 <emersondowning@gmail.com>
2020-05-27 15:57:53 -0400

    finished styling and everything working besides the search function

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,10 @@ import React from 'react'
 function Footer (){
   return (
     <div className="footer">
-      <p>Random Employee Info was provided by ...</p>
-      <p>This application was built by <a href="https://github.com/EDowning2000">Emerson Downing</a> </p>
+      <p>Random Employee Info was provided by
+        <a href="https://randomuser.me/"> randomuser.me</a>
+      </p>
+      <p>Find me on <a href="https://github.com/EDowning2000">Github</a> </p>
       <a href="https://github.com/EDowning2000/employee-tracker">This projects source Code.</a>
     </div>
   )
